0ba237230fb17fe26cafe751b16bafdb0e3fc459
arutinn <slavik_arutin@rambler.ru>
2018-12-18 17:02:38 +0200

    add profile branch

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   get '/api/:hash', to: 'api/avatars#show'
 
 	post 'profile', to: 'users#edit'
-  post 'profile/add_number', to: 'users#add_number'
-  get 'profile/add_number', to: 'users#add_number'
   delete 'profile', to: 'users#destroy'
   get 'profile', to: 'users#show', as: "show_profile"
 
