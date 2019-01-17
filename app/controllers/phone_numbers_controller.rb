6aff1c51815c52e04e782ed5b19e03e526c157d2
luba.t <luba@t.me>
2019-01-17 18:50:09 +0200

    more style

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -10,8 +10,10 @@ class PhoneNumbersController < ApplicationController
 
   def update
     @phones = current_user.phones.order(:id)
+    binding.pry
     return unless user_phone
     user_phone.update(phone_params)
+    puts user_phone.errors.inspect
   end
 
   def destroy
