faca1ba48f2a357328023a387ae140881a2f5c6d
luba.t <luba@t.me>
2019-01-18 21:50:29 +0200

    update gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,14 @@ class User < ApplicationRecord
   has_many :avatars
   
   def auto_create_gravatar
+    hash = Digest::MD5.hexdigest self.email
+    url = "https://www.gravatar.com/avatar/#{hash}"
     unless self.email.blank?
       unless self.avatars.where("url like ?", "%gravatar%").exists?
-        hash = Digest::MD5.hexdigest self.email
-        url = "https://www.gravatar.com/avatar/#{hash}"
         self.avatars.create(url: url)
+      else
+        grava = self.avatars.where("url like ?", "%gravatar%").take
+        grava.update(url: url)
       end
     end
   end
