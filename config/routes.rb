bb86536ee0f87c4c5db1528bcde9cde7fe656ea4
galulex <galulex@gmail.com>
2019-01-21 19:48:25 +0200

    Fixes

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,12 @@ Rails.application.routes.draw do
   post 'profile', to: 'users#edit'
   delete 'profile', to: 'users#destroy'
   get 'profile', to: 'users#show', as: 'show_profile'
-  patch 'phone_numbers', to: 'phone_numbers#update'
-
-  patch "/phone_numbers", to: "phone_numbers#update"
-  patch "/phone_numbers/:id", to: "phone_numbers#confirm_number"
 
   resources :phone_numbers, only: %i[new create update destroy] do
     patch '/', to: 'phone_numbers#updates', on: :collection
+    get 'confirm', to: 'phone_numbers#confirm_number', on: :member, as: :confirm
   end
-  get '/:token/confirm_email/', :to => "users#update", as: 'confirm_email'
+  get '/:token/confirm_email/', to: 'users#update', as: 'confirm_email'
 
   resources :avatars, only: %i[create destroy]
 end
