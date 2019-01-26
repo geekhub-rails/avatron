f84400e297393f399ab1b8c7b5dff7b13b20e3c6
luba.t <luba@t.me>
2019-01-26 22:30:38 +0200

    finish styling

--- app/assets/javascripts/design.js
+++ app/assets/javascripts/design.js
@@ -8,4 +8,12 @@ $(function(){
     $(this).parents('.container').find('.orange-border').removeClass('orange-border')    
   })
 
+  $('body').on( "click", ".login, .login-string", function(){
+    $(this).parents('.container').find(".login-string").removeClass('orange-border') 
+    $(this).parents('.container').find(".login").removeClass('orange-back') 
+    $(this).parents('form .row').find(".login-string").addClass('orange-border')   
+    $(this).parents('form .row').find(".login").addClass('orange-back')
+    console.log($(this))
+  })
+
 });
